f057d45e503a28cd25518cd9f9534908d96458e8
rajat-garg-dev <gargrajat781@gmail.com>
2024-02-14 16:49:33 +0530

    Merged my frontend full working installing socket.io-client and the react-scroll-to-bottom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,66 @@
+import { useState } from "react";
 import "./App.css";
-
+import io from "socket.io-client";
+import Chat from "./Chat";
+const socket = io.connect("http://localhost:3001");
 function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+  const join = () => {
+    if (name !== "" && password === "123") {
+      socket.emit("join", name);
+      setShow(true);
+    } else if (name === "" || password === "") {
+      const p = document.getElementById("error");
+      p.innerText = "Enter the field ";
+    } else if (password !== "123") {
+      const p = document.getElementById("error");
+      p.innerText = "Password is incorrect ";
+    }
+  };
+  console.log(password, "pass");
   return (
     <div className="App">
-      <h1 className="text-red-500">hello</h1>
+      <div className="flex justify-center">
+        {!show ? (
+          <div className="bg-sky-400 flex flex-col gap-[4px] w-[300px] p-[20px] rounded-[4px] items-center">
+            <h1 className="text-white font-[800] text-[30px] ">
+              Start Chatting
+            </h1>
+            <div className=" flex flex-col gap-[1px]">
+              <label className="text-white font-[500]">Name:</label>
+              <input
+                className="rounded-[4px] px-[4px]"
+                type="text"
+                placeholder="Enter your Name"
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-[1px]">
+              <label className="text-white font-[500]">Password:</label>
+              <input
+                className="rounded-[4px] px-[4px]"
+                type="text"
+                placeholder="Enter your Name"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+            </div>
+            <button
+              className="border-[4px] w-[120px] mx-auto px-[4px] mt-2 text-white font-[600] rounded-[4px]"
+              onClick={join}>
+              Join
+            </button>
+            <p className="text-red-600" id="error"></p>
+          </div>
+        ) : (
+          <Chat socket={socket} name={name} />
+        )}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/Chat.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+import chaticon from "./Images/chaticon.jpg";
+export default function Chat({ socket, name }) {
+  const [currentmessage, setCurrentMessage] = useState("");
+  const [messagelist, setMessagelist] = useState([]);
+  const SendMessage = async () => {
+    if (currentmessage !== "") {
+      const messageData = {
+        name: name,
+        message: currentmessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessagelist((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+  useEffect(() => {
+    socket.on("recive_message", (data) => {
+      setMessagelist((list) => [...list, data]);
+    });
+  }, [socket]);
+  console.log(messagelist, "message ");
+  return (
+    <div className="w-[100%] h-[100%] flex flex-col justify-between  bg-black">
+      {/* Header */}
+      <div className="bg-[#f6265a] max-md:p-2 h-[5vh] px-2 flex gap-2 ">
+        <img height={32} width={32} src={chaticon} />
+        <p className="font-[800] text-white text-[18px]">Live Chat</p>
+      </div>
+      {/* body */}
+      <ScrollToBottom className="h-[90vh]  overflow-x-hidden">
+        {messagelist.map((item) => {
+          return (
+            <div
+              className={` flex px-2 py-1 ${
+                name === item.name ? "justify-end" : "justify-start"
+              }`}>
+              <div>
+                <div
+                  className={`text-white max-w-[100px] overflow-hidden rounded-[8px] w-[100%] px-2 py-[2px] ${
+                    name === item.name ? " bg-green-500 right-2" : "bg-blue-400"
+                  }`}>
+                  <p>{item.message}</p>
+                </div>
+                <div
+                  className={`text-gray-300 text-[12px] ${
+                    name === item.name ? "flex-row-reverse" : "flex"
+                  } flex gap-3`}>
+                  <p>{name === item.name ? <p>you</p> : <p>{item.name}</p>}</p>
+                  <p>{item.time}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </ScrollToBottom>
+
+      {/* footer */}
+      <div className="w-[100%] h-[5vh] gap-2 px-2 flex ">
+        <input
+          className="w-[100%] px-2 rounded-[4px] py-[1px]"
+          type="text"
+          value={currentmessage}
+          placeholder="hey.."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && SendMessage();
+          }}
+        />
+        <button
+          className="bg-green-400 text-white px-2 font-[600] rounded-[4px] py-[1px]"
+          onClick={SendMessage}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
